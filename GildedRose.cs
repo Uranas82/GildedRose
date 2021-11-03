1ec19b8e76304c7b7d7ccee88b49fbf9fdbd01e8
Algimantas <grabliauskas.a@gmail.com>
2021-11-03 19:13:29 +0200

    Final refactoring

---
+++ GildedRose.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace GildedRose
+{
+    public class GildedRose
+    {
+        IList<Item> Items;
+
+        public GildedRose(IList<Item> Items)
+        {
+            this.Items = Items;
+        }
+
+        public void UpdateQuality()
+        {
+            foreach (var item in Items)
+            {
+                var itemProcessor = item.MapToItemProcessor();
+
+                itemProcessor.UpdateQuality(item);
+            }
+        }
+    }
+}
---
+++ Processors/SulfurasItemProcessor.cs
@@ -0,0 +1,10 @@
+﻿namespace GildedRose.Processors
+{
+    public class SulfurasItemProcessor : BaseItemProcessor
+    {
+        public override void UpdateQuality(Item item)
+        {
+            // “Sulfuras”, being a legendary item, never has to be sold or decreases in Quality;
+        }
+    }
+}
